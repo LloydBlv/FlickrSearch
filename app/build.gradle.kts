b49aa8db728d424dce24cd5d2e221e3163d4fb37
Mohammad Reza Najafi <mohammad.rezanajafi@nentgroup.com>
2021-03-19 20:06:32 +0330

    Adds Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(Config.compileSdkVersion)
+    buildToolsVersion(Config.buildToolsVersion)
+
+    defaultConfig {
+        applicationId = "ir.zinutech.android.flickrsearch"
+        minSdkVersion(Config.minSdkVersion)
+        targetSdkVersion(Config.targetSdkVersion)
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+        testInstrumentationRunner = Config.androidTestInstrumentation
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dependencies.appLibraries)
+}
---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,14 @@
+object Config {
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
+
+    const val buildToolsVersion = "30.0.3"
+
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val applicationId = "ir.zinutech.android.flickrsearch"
+
+    const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
+}
